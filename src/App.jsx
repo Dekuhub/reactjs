5b0e55925b39e586462ca2388913f2b0fbe7aaae
Deku <remics2006@bk.ru>
2025-02-04 17:25:08 +0500

    Первоначальный коммит

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { Button , H1 , Main } from './assets/Component/components'
+// function App() {
+//   return (
+//     <>
+// <H1 text="Нет H2"/>
+//  <Main>
+
+// <Button text="Вру H1" />
+
+//  </Main> 
+//     </>
+//   )
+
+// }
+
+
+export default App
+
---
+++ src/assets/Component/components.jsx
@@ -0,0 +1,21 @@
+// import classes from './component.module.css'
+// export const Button = ({children}) => {
+//     return (
+//         <button className={classes.button}>
+//             {children}
+//         </button>
+//     )
+// }
+
+// export const H1 = function({text="trem"}) {
+//     return (
+//         <h1>{text}</h1>
+//     )
+// } 
+// export const Main = ({children}) => {
+//     return (
+//         <main className={classes.main}>
+//             {children}
+//         </main>
+//     )
+// }
