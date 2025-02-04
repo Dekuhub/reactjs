7c784ad8da4c5ede4b185cb598cf21ebab853f67
Deku <remics2006@bk.ru>
2025-02-04 17:33:43 +0500

    s22

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import './App.css'
 import { Button , H1 , Main } from './assets/Component/components'
-// function App() {
-//   return (
-//     <>
-// <H1 text="Нет H2"/>
-//  <Main>
+function App() {
+  return (
+    <>
+<H1 text="Нет H2"/>
+ <Main>
 
-// <Button text="Вру H1" />
+<Button text="Вру H1" />
 
-//  </Main> 
-//     </>
-//   )
+ </Main> 
+    </>
+  )
 
-// }
+}
 
 
 export default App
